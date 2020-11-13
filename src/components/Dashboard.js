6a7105fa39ffe86128c111e8b387816914f3e0c8
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-13 13:01:27 -0800

    okay some things are working

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
 export class Dashboard extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      myPosts: true
+    }
+  }
   render() {
     return (
       <div>
         <h1>Dashboard</h1>
+        <label for="search">Search</label>
+        <input type="text"/>
+        <button>Search</button>
+        <button>Reset</button>
+
+        <input type="checkbox"/>
+        <label for="myPosts">My Posts</label>
+
       </div>
     )
   }
