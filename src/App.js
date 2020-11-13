62fdb2fdeaad0a6836f1506dddea6c054cae03c4
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-13 08:26:34 -0800

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Auth from './components/Auth';
+import Dashboard from './components/Dashboard';
+import Form from './components/Form';
+import Post from './components/Post';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Nav/>
+     <Auth/>
+     <Dashboard/>
+     <Form/>
+     <Post/>
+
     </div>
   );
 }
